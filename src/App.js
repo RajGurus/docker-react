0bbb1637758dc0cff55865cfb3df311fba318034
Rajkumar <rajguru@arrcus.com>
2020-08-07 19:47:24 -0700

    Update to src/App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          moving to docker-compose ..... again
+          Changing to AWS deployment
         </p>
         <a
           className="App-link"
